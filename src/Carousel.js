ef9fc705721d5c5274865caa2b31c729b1b0a77d
Zach Sexton <zsexton2011@gmail.com>
2022-10-16 13:05:28 -0400

    Add carousel for details page

---
+++ src/Carousel.js
@@ -0,0 +1,43 @@
+import { Component } from 'react';
+
+class Carousel extends Component {
+  state = {
+    active: 0,
+  };
+
+  static defaultProps = {
+    images: ['http://pets-images.dev-apis.com/pets/none.jpg'],
+  };
+
+  handleIndexClick = (event) => {
+    this.setState({
+      active: +event.target.dataset.index, //unary +, it just converts whatever the value is to a number (in this case a string). could use int.parse instead
+    });
+  };
+
+  render() {
+    const { active } = this.state;
+    const { images } = this.props;
+
+    return (
+      <div className="carousel">
+        <img src={images[active]} alt="animal" />
+        <div className="carousel-smaller">
+          {images.map((photo, index) => (
+            // eslint-disable-next-line
+            <img
+              onClick={this.handleIndexClick}
+              key={photo}
+              src={photo}
+              data-index={index}
+              className={index === active ? 'active' : ''}
+              alt="animal thumbnail"
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Carousel;
--- src/Details.js
+++ src/Details.js
@@ -1,9 +1,54 @@
+import { Component } from 'react';
 import { useParams } from 'react-router-dom';
+import Carousel from './Carousel';
 
-const Details = () => {
-  const { id } = useParams();
+class Details extends Component {
+  //   constructor(props) {
+  //     super(props);
 
-  return <h2>{id}</h2>;
+  //     this.state = { loading: true };
+  //   }
+
+  //javascript properties proposal plugin allows this instead of constructor if you're doing basic property setting
+  state = { loading: true };
+
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.params.id}`
+    );
+    const json = await res.json();
+
+    this.setState({ loading: false, ...json.pets[0] });
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <h2>loading ... </h2>;
+    }
+
+    const { animal, breed, city, state, description, name, images } =
+      this.state;
+
+    return (
+      <div className="details">
+        <Carousel images={images} />
+        <div>
+          <h1>{name}</h1>
+          <h2>
+            {animal} - {breed} - {city}, {state}
+          </h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+const WrappedDetails = () => {
+  const params = useParams();
+
+  return <Details params={params} />;
 };
 
-export default Details;
+export default WrappedDetails;
