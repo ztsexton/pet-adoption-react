bea4ea95662b85f289f1ae6c8920844717c2959e
Zach Sexton <zsexton@DESKTOP-1OS29V4.localdomain>
2022-10-16 00:20:22 -0400

    Add details page with no logic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import { render } from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { StrictMode } from 'react';
 import SearchParams from './SearchParams';
+import Details from './Details';
 
 const App = () => {
   return (
     <StrictMode>
       <div id="my-app">
-        <h1>Adopt Me!</h1>
-        <SearchParams />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/details/:id" element={<Details />} />
+            <Route path="/" element={<SearchParams />} />
+          </Routes>
+        </BrowserRouter>
       </div>
     </StrictMode>
   );
---
+++ src/Details.js
@@ -0,0 +1,5 @@
+const Details = () => {
+  return <h2>hi omg lol</h2>;
+};
+
+export default Details;
